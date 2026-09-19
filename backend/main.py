0a3dae110dd40f41f6282f78165aa090b7c09171
Yash <yashmay15@gmail.com>
2026-09-20 01:08:14 +0530

    vite issue

--- backend/main.py
+++ backend/main.py
@@ -36,7 +36,7 @@ app = FastAPI(title="DSA Visualizer API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173","https://dsa-visualiser-zeta.vercel.app/"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173","https://dsa-visualiser-zeta.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
